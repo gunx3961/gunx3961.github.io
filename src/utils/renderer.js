be1f4c1959dcd393c0bdcb2c160811dc47f57080
Gunx <gunx@foxmail.com>
2017-06-13 04:32:57 +0800

    Add language identifier to code blocks

--- src/utils/renderer.js
+++ src/utils/renderer.js
@@ -1,5 +1,43 @@
 import marked from 'marked';
 
+const hljs = window.hljs;
+
+// override original renderer method of code blocks
+// specify className to <pre />s
+const renderer = new marked.Renderer();
+function escape(html, encode) {
+  return html
+    .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
+renderer.code = function (code, lang, escaped) {
+  if (this.options.highlight) {
+    const out = this.options.highlight(code, lang);
+    if (out != null && out !== code) {
+      escaped = true;
+      code = out;
+    }
+  }
+
+  if (!lang) {
+    return `<pre><code>${
+      escaped ? code : escape(code, true)
+      }\n</code></pre>`;
+  }
+
+  const className = `${this.options.langPrefix}${escape(lang, true)}`;
+
+  return `<pre class="${className}"><code class="${className}">${
+    escaped ? code : escape(code, true)
+    }\n</code></pre>\n`;
+};
+
+marked.setOptions({ renderer });
+
 function AsyncHighlight(lang, value) {
   return new Promise((resolve) => {
     if (!hljs.getLanguage(lang)) resolve(value);
